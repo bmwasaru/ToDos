8a4248009fab578d0ae53fadf29b1bda95637098
britone mwasaru <bmwasaru@gmail.com>
2021-01-15 13:11:17 +0300

    Initial Commit

---
+++ Todos/TodosApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodosApp.swift
+//  Todos
+//
+//  Created by Britone Mwasaru on 15/01/2021.
+//
+
+import SwiftUI
+
+@main
+struct TodosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
